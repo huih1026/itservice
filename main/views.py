495ae5d0aa7d03e316fd8bbdea2cae56d346a74f
Hui <huih1026@gmail.com>
2025-01-01 17:20:05 -0800

    added two form to compare

--- main/views.py
+++ main/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 
 
 def home(request):
-  context = {'key': "first message"}
-  return render(request, 'main/home.html', context)
+    context = {'key': "first message"}
+    return render(request, 'main/home.html', context)
